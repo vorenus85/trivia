8dce908502a516878ea8ffacd538a16a6a966fa2
Janos Perge <janos.perge@gmail.com>
2024-11-08 00:23:25 +0100

    add deploy script

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.VITE_BASE_URL,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
